1e19b597ecdc8531a01130d840763c1d9ea5e5b4
yamaff <fuga.19671028@icloud.com>
2024-11-05 11:06:32 +0900

    名前

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from fastapi.responses import JSONResponse
 app = FastAPI()
 
 
-@app.get("/name")  # ルーティングの設定
+@app.get("/{name}")  # ルーティングの設定
 def root(name: str):
     return JSONResponse(content={"Hello": name})

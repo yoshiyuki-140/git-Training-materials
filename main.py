c1a0da7fb84991a40bffc324861b11328d70b0d2
yamaff <fuga.19671028@icloud.com>
2024-11-07 10:49:21 +0900

    event

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-#from fastapi import FastAPI
-#from fastapi.responses import FileResponse
-#import os
-#app = FastAPI()
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+import os
+app = FastAPI()
 
-#@app.get("/")  # ルーティングの設定
-#def root():
-    #return {"":""}
+@app.get("/")  # ルーティングの設定
+def root():
+    return {"":""}
 
-#@app.get("/hello")
-#def root():
-    #return {"hello":"world"}
+@app.get("/hello")
+def root():
+    return {"hello":"world"}
 
-#@app.get("/index")
-#def root():
-    #url = os.path.join("git-Training-materials", "index.html")
-    #return FileResponse(url)
+@app.get("/index")
+def root():
+    url = os.path.join("git-Training-materials", "index.html")
+    return FileResponse(url)
 
-#@app.get("/{name}")
-#def root(name: str):
-    #return {"Hello": name}
+@app.get("/{name}")
+def root(name: str):
+    return {"Hello": name}
